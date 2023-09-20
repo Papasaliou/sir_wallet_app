62c5d8499fc314f8d18a1083b795ee6be2635929
Papasaliou <Pro@192.168.1.46>
2023-09-20 04:37:39 +0200

    update 1

--- src/test/java/sn/sir/wallet/sirwalletapp/model/UsersTest.java
+++ src/test/java/sn/sir/wallet/sirwalletapp/model/UsersTest.java
@@ -1,4 +1,45 @@
+package sn.sir.wallet.sirwalletapp.model;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class UsersTest {
-  
+    @Test
+    public void testUsersConstructorAndGetter()
+    {
+        int id = 1;
+        String prenom = "Khadim";
+        String nom = "Mbaye";
+        Long cni = 1996347890L;
+        Long telephone = 772345899L;
+
+        Users user = new Users(id, prenom, nom, cni, telephone);
+
+        assertEquals(id, user.getId());
+        assertEquals(prenom, user.getPrenom());
+        assertEquals(nom, user.getNom());
+        assertEquals(cni, user.getcNI());
+        assertEquals(telephone, user.getTelephone());
+
+    }
+    @Test
+    public  void testUserSetters()
+    {
+        Users user = new Users();
+        user.setId(1);
+        user.setPrenom("Arame");
+        user.setNom("Faye");
+        user.setcNI(000000000001L);
+        user.setTelephone(776349900L);
+
+        assertEquals(1, user.getId());
+        assertEquals("Arame", user.getPrenom());
+        assertEquals("Faye", user.getNom());
+        assertEquals(000000000001L, user.getcNI());
+        assertEquals(776349900, user.getTelephone());
+    }
+
+
+
 }
