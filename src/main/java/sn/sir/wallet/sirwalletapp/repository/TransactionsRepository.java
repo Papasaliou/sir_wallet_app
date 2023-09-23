fa77375cdcffc916d5aafa3fec83fcce52632aa6
Papasaliou <Pro@192.168.1.46>
2023-09-24 01:12:32 +0200

    update

--- src/main/java/sn/sir/wallet/sirwalletapp/repository/TransactionsRepository.java
+++ src/main/java/sn/sir/wallet/sirwalletapp/repository/TransactionsRepository.java
@@ -1,7 +1,9 @@
 package sn.sir.wallet.sirwalletapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import sn.sir.wallet.sirwalletapp.model.Transactions;
-
+@Repository
 public interface TransactionsRepository extends JpaRepository<Transactions,Integer> {
+
 }
--- src/main/java/sn/sir/wallet/sirwalletapp/repository/UsersRepository.java
+++ src/main/java/sn/sir/wallet/sirwalletapp/repository/UsersRepository.java
@@ -1,8 +1,10 @@
 package sn.sir.wallet.sirwalletapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import sn.sir.wallet.sirwalletapp.model.Users;
-
+@Repository
 public interface UsersRepository extends JpaRepository<Users,Integer> {
+
 
 }
