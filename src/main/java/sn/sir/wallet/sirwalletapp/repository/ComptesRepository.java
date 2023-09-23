91eeef8059330060f8b0be29b1ecac2a26af03e8
Papasaliou <Pro@192.168.1.46>
2023-09-24 01:07:46 +0200

    update

--- src/main/java/sn/sir/wallet/sirwalletapp/repository/ComptesRepository.java
+++ src/main/java/sn/sir/wallet/sirwalletapp/repository/ComptesRepository.java
@@ -1,7 +1,9 @@
 package sn.sir.wallet.sirwalletapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import sn.sir.wallet.sirwalletapp.model.Comptes;
-
+@Repository
 public interface ComptesRepository extends JpaRepository<Comptes,Integer> {
+
 }
