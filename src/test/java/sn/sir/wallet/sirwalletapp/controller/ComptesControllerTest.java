91e42523fc714e97be1e437b8b2cb864600ffecb
Papasaliou <Pro@192.168.1.46>
2023-09-25 04:55:15 +0200

    second commit

--- src/test/java/sn/sir/wallet/sirwalletapp/controller/ComptesControllerTest.java
+++ src/test/java/sn/sir/wallet/sirwalletapp/controller/ComptesControllerTest.java
@@ -1,14 +1,27 @@
 package sn.sir.wallet.sirwalletapp.controller;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import sn.sir.wallet.sirwalletapp.model.Comptes;
 import sn.sir.wallet.sirwalletapp.model.Transactions;
 import sn.sir.wallet.sirwalletapp.repository.ComptesRepository;
 import sn.sir.wallet.sirwalletapp.service.ComptesService;
 
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
 @SpringBootTest
 class ComptesControllerTest {
     @Autowired
@@ -31,4 +44,21 @@ class ComptesControllerTest {
         assertNotNull(comptes);
         assertEquals(id, comptes.getId());
     }
+
+    @Test
+    public void testCreer() {
+        // Création d'un compte
+        Comptes comptes = new Comptes(19,100.0,new Date(),"Compte1",7);
+//
+        // Appel de la méthode à tester
+        comptesService.creerCompte(comptes);
+
+        // Récupération du compte créé
+        Comptes compteTrouve = comptesService.getCompteById(19);
+
+        // Vérification du résultat
+        Assertions.assertEquals(compteTrouve.getMotDePasse(), "Compte1");
+        Assertions.assertEquals(compteTrouve.getSolde(), 100.0);
+    }
+
 }
--- src/test/java/sn/sir/wallet/sirwalletapp/service/TransactionsServiceTest.java
+++ src/test/java/sn/sir/wallet/sirwalletapp/service/TransactionsServiceTest.java
@@ -1,6 +1,7 @@
 package sn.sir.wallet.sirwalletapp.service;
 
 import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,9 +11,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import sn.sir.wallet.sirwalletapp.model.Comptes;
+import sn.sir.wallet.sirwalletapp.model.Transactions;
 import sn.sir.wallet.sirwalletapp.repository.ComptesRepository;
 import sn.sir.wallet.sirwalletapp.repository.TransactionsRepository;
 import sn.sir.wallet.sirwalletapp.repository.UsersRepository;
+
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -27,26 +31,70 @@ class TransactionsServiceTest {
         this.transactionsService = transactionsService;
         this.comptesRepository=comptesRepository;
     }
-
 //    @Test
-//    public void testTransfert() {
-//        int ide=11;
-//        int idr=10;
+//    public void testCreer() {
+//        // Création d'un compte
+//        Transactions transactions = new Transactions(12,100.0,new Date(),1,13,20);
+////
+//        // Appel de la méthode à tester
+//        transactionsService.createTransaction(transactions);
 //
-//        // Création de deux objets Comptes
-//        Comptes comptes1 =comptesRepository.getReferenceById(ide);
-//        Comptes comptes2 = comptesRepository.getReferenceById(idr);
+//        // Récupération du compte créé
+//        Transactions transactionTrouvee = transactionsService.getTransaction(12);
 //
-//        // Initialisation des solde et montantTransaction
-//        double solde1 = comptes1.getSolde();
-//        double solde2 = comptes2.getSolde();
-//        double montantTransaction = 50.0;
-//        transactionsService.transfert(comptes1, comptes2, montantTransaction);
-//
-//        // Vérification des résultats
-//        assertEquals(solde1 - montantTransaction, comptes1.getSolde());
-//        assertEquals(solde2 + montantTransaction, comptes2.getSolde());
+//        // Vérification du résultat
+//        Assertions.assertEquals(transactionTrouvee.getMontant(), transactions.getMontant());
+//        Assertions.assertEquals(transactionTrouvee.getEtat(), 1);
 //    }
+
+    @Test
+    public void testTransfert() {
+        int ide=11;
+        int idr=10;
+
+        // Création de deux objets Comptes
+        Comptes comptes1 =comptesRepository.findById(ide).get();
+        Comptes comptes2 = comptesRepository.findById(idr).get();
+
+        // Initialisation des solde et montantTransaction
+        double solde1 = comptes1.getSolde();
+        double solde2 = comptes2.getSolde();
+        double montantTransaction = 50.0;
+        transactionsService.transfert(comptes1, comptes2, montantTransaction);
+
+        // Vérification des résultats
+        assertEquals(solde1 - montantTransaction, comptes1.getSolde());
+        assertEquals(solde2 + montantTransaction, comptes2.getSolde());
+    }
+    @Test
+    public void testDepopt() {
+        int id=11;
+
+        // Création de deux objets Comptes
+        Comptes comptes =comptesRepository.findById(id).get();
+        // Initialisation des solde et montantTransaction
+        double solde = comptes.getSolde();
+        double montantTransaction = 50.0;
+        transactionsService.depot(comptes, montantTransaction);
+
+        // Vérification des résultats
+
+        assertEquals(solde + montantTransaction, comptes.getSolde());
+    }
+    @Test
+    public void testRetrait() {
+        int id=11;
+        // Création de deux objets Comptes
+        Comptes comptes =comptesRepository.findById(id).get();
+        // Initialisation des solde et montantTransaction
+        double solde = comptes.getSolde();
+        double montantTransaction = 50.0;
+        transactionsService.retrait(comptes, montantTransaction);
+
+        // Vérification des résultats
+
+        assertEquals(solde - montantTransaction, comptes.getSolde());
+    }
 
 
 }
